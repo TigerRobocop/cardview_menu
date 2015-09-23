ef611168293c88d3ecdd1cc93b4ef3843d4ba8c1
LivSouza <liviarsouza@gmail.com>
2015-09-23 00:30:02 -0300

    cardview menu for lvs app

---
+++ app/src/main/java/com/training/lsouza/lvsmenu/MainActivity.java
@@ -0,0 +1,25 @@
+package com.training.lsouza.lvsmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
